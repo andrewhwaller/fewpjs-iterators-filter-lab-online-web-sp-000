d7063bfa324db3f6adc98bdeb82c5661bd0cedbe
Andrew Waller <andrewhwaller@gmail.com>
2019-10-01 23:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function findMatching(drivers, string) {
     return name.toLowerCase() === string.toLowerCase();
   })
 }
+
+function fuzzyMatch(drivers, string) {
+  let nameLength = string.length;
+  return drivers.filter(function (name) {
+    return name.slice(0, nameLength) === string;
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  });
+}
